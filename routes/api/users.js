d519562570b82444d8d7756b6dc4f8bb24c5819d
maverink <juliomaurocarvalho@hotmail.com>
2019-01-19 23:17:34 +0000

    encrypt pass before saving on DB with bcrypt

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
 
 const User = require("../../models/User");
 const gravatar = require("gravatar");
@@ -13,7 +14,6 @@ router.get("/test", (req, res) => {
 router.post("/register", (req, res) => {
   User.findOne({ email: req.body.email }).then(user => {
     if (user) {
-      console.log(user);
       return res.status(404).json({ msg: "email already registered" });
     } else {
       const avatar = gravatar.url(req.body.email, {
@@ -27,8 +27,20 @@ router.post("/register", (req, res) => {
         password: req.body.email,
         avatar
       });
-      newUser.save();
-      res.json({ msg: "you ve been registered" });
+
+      bcrypt.hash(newUser.password, 10, (err, hash) => {
+        if (err) throw err;
+
+        newUser.password = hash;
+        newUser
+          .save()
+          .then(user => {
+            res.json({ user });
+          })
+          .catch(err => {
+            console.log(err);
+          });
+      });
     }
   });
 });
