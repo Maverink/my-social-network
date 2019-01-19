8435e9f2146653a268abb11ffaf527eb8f432500
maverink <juliomaurocarvalho@hotmail.com>
2019-01-19 14:37:37 +0000

    connected db to mongoose

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+  mongoURI: "mongodb://Julio:a1a1a1@ds261644.mlab.com:61644/my-social-network"
+};
--- server.js
+++ server.js
@@ -2,6 +2,19 @@ const express = require("express");
 
 const app = express();
 
+const mongoose = require("mongoose");
+
+const db = require("./config/keys").mongoURI;
+
+mongoose
+  .connect(db)
+  .then(() => {
+    console.log("connected");
+  })
+  .catch(err => {
+    console.log(err);
+  });
+
 app.get("/", (req, res) => {
   res.send("ok");
 });
