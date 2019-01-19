6383d6ab8a25971b535067b0fb1d6f4de23c013c
maverink <juliomaurocarvalho@hotmail.com>
2019-01-19 18:34:20 +0000

    done User model and created Schema

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const router = express.Router();
 
+//@route    GET api/osts/test
+//@desc     Tests posts Route
+//@access   Public
 router.use("/test", (req, res) => {
   res.json({ msg: "posts test working!" });
 });
--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const router = express.Router();
 
+//@route    GET api/users/test
+//@desc     Tests user Route
+//@access   Public
 router.get("/test", (req, res) => {
   res.json({ msg: "user test working!" });
 });
